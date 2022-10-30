1cb65a77a70d908f03948c2ba5d93b6147f1c58c
Abraham Hoogendoorn <17817975+arabham@users.noreply.github.com>
2022-10-30 15:37:46 -0500

    created project

---
+++ graphics/mytiles/enviro.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="enviro" tilewidth="128" tileheight="128" tilecount="11" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="128" source="../objects/14.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="128" source="../objects/13.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../objects/05.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../objects/06.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../objects/07.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../objects/09.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../objects/10.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../objects/18.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../objects/19.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../objects/20.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../objects/0.png"/>
+ </tile>
+</tileset>
